5e70317d31c40ee448b086151c743e2f782a5495
yannan66 <1032718055@qq.com>
2018-10-05 17:00:52 +0800

    新增学生实体类

---
+++ src/Models/Student.java
@@ -0,0 +1,14 @@
+package Models;
+
+public class Student {
+
+	private  String userName;
+	
+	public String GetUserName(){
+		return this.userName;
+	}
+	
+	public void SetUserName(String value){
+		this.userName=value;
+	}
+}
